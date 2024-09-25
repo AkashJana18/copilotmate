40611c0d1dc11696fbc4533ab7e44a4408b7030d
AkashJana18 <akashjana663@gmail.com>
2024-09-26 01:14:38 +0530

    stage 0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <div className="text-center">
-      CopilotMate
+      <Hero />
     </div>
   );
 }
