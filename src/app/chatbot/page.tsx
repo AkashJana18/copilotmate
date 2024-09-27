5dcc86a1862178bfe9efd9498e96fbf5afaae1ca
AkashJana18 <akashjana663@gmail.com>
2024-09-27 15:22:35 +0530

    chatbot

--- src/app/chatbot/page.tsx
+++ src/app/chatbot/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { CopilotChat } from "@copilotkit/react-ui";
+import "@copilotkit/react-ui/styles.css";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="p-10 m-10">
+      <CopilotChat
+        labels={{
+          title: "Your Assistant",
+          initial: "Hi! 👋 How can I assist you today?",
+        }}
+      />
+    </div>
+  );
+};
 
-export default page
+export default page;
