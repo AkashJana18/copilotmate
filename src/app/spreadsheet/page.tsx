81ec906159acd166f0577bfba0721b00d0260231
AkashJana18 <akashjana663@gmail.com>
2024-09-30 21:04:06 +0530

    spreadsheet page

---
+++ src/app/spreadsheet/page.tsx
@@ -0,0 +1,131 @@
+"use client";
+import "@copilotkit/react-ui/styles.css";
+
+import React, { useState } from "react";
+import Sidebar from "@/components/Spreadsheet/Sidebar";
+import SingleSpreadsheet from "@/components/Spreadsheet/SingleSpreadsheet";
+import {
+  CopilotKit,
+  useCopilotAction,
+  useCopilotReadable,
+} from "@copilotkit/react-core";
+import { CopilotSidebar } from "@copilotkit/react-ui";
+import { INSTRUCTIONS } from "./instructions";
+import { canonicalSpreadsheetData } from "@/components/Spreadsheet/canonicalSpreadsheetData";
+import { SpreadsheetData } from "@/components/Spreadsheet/type";
+import { PreviewSpreadsheetChanges } from "@/components/Spreadsheet/PreviewSpreadsheetChanges";
+
+const HomePage = () => {
+  return (
+    <CopilotKit runtimeUrl="/api/copilotkit">
+      <CopilotSidebar
+        instructions={INSTRUCTIONS}
+        labels={{
+          initial: "Welcome to the spreadsheet app! How can I help you?",
+        }}
+        defaultOpen={true}
+        clickOutsideToClose={false}
+      >
+        <Main />
+      </CopilotSidebar>
+    </CopilotKit>
+  );
+};
+
+const Main = () => {
+  const [spreadsheets, setSpreadsheets] = React.useState<SpreadsheetData[]>([
+    {
+      title: "Spreadsheet 1",
+      rows: [
+        [{ value: "" }, { value: "" }, { value: "" }],
+        [{ value: "" }, { value: "" }, { value: "" }],
+        [{ value: "" }, { value: "" }, { value: "" }],
+      ],
+    },
+  ]);
+
+  const [selectedSpreadsheetIndex, setSelectedSpreadsheetIndex] = useState(0);
+
+  useCopilotAction({
+    name: "createSpreadsheet",
+    description: "Create a new  spreadsheet",
+    parameters: [
+      {
+        name: "rows",
+        type: "object[]",
+        description: "The rows of the spreadsheet",
+        attributes: [
+          {
+            name: "cells",
+            type: "object[]",
+            description: "The cells of the row",
+            attributes: [
+              {
+                name: "value",
+                type: "string",
+                description: "The value of the cell",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "title",
+        type: "string",
+        description: "The title of the spreadsheet",
+      },
+    ],
+    render: (props) => {
+      const { rows, title } = props.args;
+      const newRows = canonicalSpreadsheetData(rows);
+
+      return (
+        <PreviewSpreadsheetChanges
+          preCommitTitle="Create spreadsheet"
+          postCommitTitle="Spreadsheet created"
+          newRows={newRows}
+          commit={(rows) => {
+            const newSpreadsheet: SpreadsheetData = {
+              title: title || "Untitled Spreadsheet",
+              rows: rows,
+            };
+            setSpreadsheets((prev) => [...prev, newSpreadsheet]);
+            setSelectedSpreadsheetIndex(spreadsheets.length);
+          }}
+        />
+      );
+    },
+    handler: ({ rows, title }) => {
+      // Do nothing.
+      // The preview component will optionally handle committing the changes.
+    },
+  });
+
+  useCopilotReadable({
+    description: "Today's date",
+    value: new Date().toLocaleDateString(),
+  });
+
+  return (
+    <div className="flex">
+      <Sidebar
+        spreadsheets={spreadsheets}
+        selectedSpreadsheetIndex={selectedSpreadsheetIndex}
+        setSelectedSpreadsheetIndex={setSelectedSpreadsheetIndex}
+      />
+      <SingleSpreadsheet
+        spreadsheet={spreadsheets[selectedSpreadsheetIndex]}
+        setSpreadsheet={(spreadsheet) => {
+          setSpreadsheets((prev) => {
+            console.log("setSpreadsheet", spreadsheet);
+            const newSpreadsheets = [...prev];
+            newSpreadsheets[selectedSpreadsheetIndex] = spreadsheet;
+            return newSpreadsheets;
+          });
+        }}
+      />
+    </div>
+  );
+};
+
+export default HomePage;
